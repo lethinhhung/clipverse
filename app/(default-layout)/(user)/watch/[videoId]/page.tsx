9f46487f18dedfed20335f973f4ef4fb7d1f06f3
KongTrua <thung260803@gmail.com>
2025-05-12 17:23:07 +0700

    feat: add management and user pages with layout components

---
+++ app/(default-layout)/(user)/watch/[videoId]/page.tsx
@@ -0,0 +1,8 @@
+export default function WatchPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-2xl font-bold">Watch Page</h1>
+      <p className="text-lg">This is the Watch page.</p>
+    </div>
+  );
+}
