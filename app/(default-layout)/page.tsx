ee09575c722b7f1224fb9b67a182f9e5e9125c1d
KongTrua <thung260803@gmail.com>
2025-05-20 19:39:23 +0700

    feat: add report dialog component and integrate select and textarea UI components

--- app/(default-layout)/page.tsx
+++ app/(default-layout)/page.tsx
@@ -1,4 +1,7 @@
 import { DarkModeToggle } from "@/components/dark-mode-toggle";
+import { ReportDialog } from "@/components/report-dialog";
+import { Button } from "@/components/ui/button";
+import { TriangleAlert } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,7 +18,7 @@ export default function Home() {
             priority
           />
 
-          <Image 
+          <Image
             className="dark:invert"
             src="/imgs/logo-default.svg"
             width={80}
@@ -38,6 +41,11 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <ReportDialog asChild userId="123" isFeedback>
+            <Button>
+              <TriangleAlert />
+            </Button>
+          </ReportDialog>
           <DarkModeToggle />
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
